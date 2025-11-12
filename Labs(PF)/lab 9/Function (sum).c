05f81094a3f72fb6e7857cd966f874f7bc242d2d
Syed Ali Ahmed <2005.syed.ahmed@gmail.com>
2025-11-13 00:24:26 +0500

    Add files via upload

---
+++ Labs(PF)/lab 9/Function (sum).c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+//function prototype
+int sum(int, int);
+//function defination
+int sum( int x, int y)
+{  
+ printf("The sum is %d",x+y);
+ return x+y;
+}
+
+
+int main()
+{
+	 int a=1;
+	 int b=2;
+     int sum(a,b);
+	 int a1=13;
+	 int b2=26;
+	 int sum(a1,b1);
+	
+	 return 0;
+}
---
+++ Labs(PF)/lab 9/function(3functions).c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void good_morning();
+void good_afternoon();
+void good_evening();
+
+
+void good_morning()
+{
+	printf("good morning\n");
+}
+void good_afternoon()
+{
+ printf("good evening\n");	
+}
+void good_evening()
+{
+	printf("good afternoon\n");
+}
+
+int main()
+{
+	 void good_morning();
+	 void good_afternoon();
+     void good_evening();
+	 
+	 return 0;
+}
